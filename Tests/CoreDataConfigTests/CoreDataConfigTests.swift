8ac2cd80521e5d7a2c9f34e5c707b808ff85f7d5
Kyeongtae Nam <vituscokr@gmail.com>
2022-09-16 14:03:19 +0900

    Initial Commit

---
+++ Tests/CoreDataConfigTests/CoreDataConfigTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CoreDataConfig
+
+final class CoreDataConfigTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CoreDataConfig().text, "Hello, World!")
+    }
+}
