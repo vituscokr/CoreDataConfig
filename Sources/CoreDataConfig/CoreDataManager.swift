423509f69d1ba59a58d8de8350acc25252a6b0f8
Kyeongtae Nam <vituscokr@gmail.com>
2022-09-16 14:24:54 +0900

    a

--- Sources/CoreDataConfig/CoreDataManager.swift
+++ Sources/CoreDataConfig/CoreDataManager.swift
@@ -10,6 +10,11 @@ import CoreData
 public class CoreDataManager: NSPersistentContainer {
     public static let shared = CoreDataManager()
     public init() {
+        let objectModelURL1 = Bundle.main.url(forResource: "CoreDataConfig", withExtension: "xcdatamodeld")
+        print(objectModelURL1)
+        let objectModel1 = NSManagedObjectModel(contentsOf: objectModelURL)
+       print(objectModel1)
+        
         guard let objectModelURL = Bundle.main.url(forResource: "CoreDataConfig", withExtension: "xcdatamodeld"),
               let objectModel = NSManagedObjectModel(contentsOf: objectModelURL) else {
             fatalError("Failed to retrivew the object model")
